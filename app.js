9a2c69307ec0e0c412eff051c582f6208c5222fd
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-17 11:09:59 -0700

    created my let state variables

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const playerHPel = document.querySelector('#player-health');
 const playerImgEl = document.querySelector('#player-img');
 const form = document.querySelector('form');
 const enemyListEl = document.querySelector('.enemies');
+
 // let state
+let playerHP = 10;
+let enemiesDefeatedCount = 0;
+let enemies = [
+  { id: 1, name: 'Wretched Ogre', hp: 3 },
+  { id: 2, name: 'Foul Mutant', hp: 2 },
+];
+let currentId = 3;
+
+
 
 // set event listeners 
   // get user input
