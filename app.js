4df05b53fda7f20d3d1f878471f707e73d2409e1
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-17 12:10:26 -0700

    create form event listener that works on submit.

--- app.js
+++ app.js
@@ -11,16 +11,35 @@ const enemyListEl = document.querySelector('.enemies');
 // let state
 let playerHP = 10;
 let enemiesDefeatedCount = 0;
+let currentId = 3;
 let enemies = [
     { id: 1, name: 'Wretched Ogre', hp: 3 },
     { id: 2, name: 'Foul Mutant', hp: 2 },
     { id: 3, name: 'Rotting Behemoth', hp: 6 },
 ];
-let currentId = 3;
+
+// set event listeners
+// new goblin form
+form.addEventListener('submit', (e) => {
+    e.preventDefault;
+
+    const data = new FormData(form);
+
+    const enemyName = data.get('enemy-name');
+
+    const newEnemy = {
+        id: currentId,
+        name: enemyName,
+        hp: Math.ceil(Math.random() * 5),
+    };
+    currentId++;
+
+  //Add obj to array of goblins in state
+    enemies.push(newEnemy);
+});
 
 
 
-// set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
